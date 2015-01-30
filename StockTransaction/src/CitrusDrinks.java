210e0d9ca851549c8563962a644c235559d845c5
iis1994 <isandoval2@mail.stmarytx.edu>
2015-01-29 22:10:05 -0600

    complete assignment

---
+++ StockTransaction/src/CitrusDrinks.java
@@ -0,0 +1,13 @@
+
+public class CitrusDrinks {
+
+	public static void main(String[] args) {
+		int original = 12467;
+		double energy = original * .14;
+		double citrus = energy * .64;
+		
+		System.out.println("Drink Energy drinks = " +energy);
+		System.out.println("Prefer citrys drinks = " +citrus);
+	}
+
+}
