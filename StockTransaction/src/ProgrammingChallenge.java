fc5c1d829be59a33d5fb63c361ecb5bbf48c059e
iis1994 <isandoval2@mail.stmarytx.edu>
2015-01-29 22:10:48 -0600

    complete assignment

---
+++ StockTransaction/src/ProgrammingChallenge.java
@@ -0,0 +1,12 @@
+
+public class ProgrammingChallenge {
+
+	public static void main(String[] args){
+		String name = "Ileana";
+		int age = 20;
+		double annualPay = 50000;
+		
+		System.out.println("My name is " +name  + "My age is " +age);
+		System.out.println("I hope to earn " +annualPay + " per year");
+	}
+}
---
+++ StockTransaction/src/assignment1.java
@@ -0,0 +1,13 @@
+
+public class assignment1 {
+	public static void main(String[] args) {
+		
+		int radius = 5;
+		double area = Math.PI*r*r;
+		int diameter = 2 * radius;
+		
+		System.out.println("The circle's radius is 5, the area is  " +area);
+		System.out.println("The diameter is " +diameter);
+	}
+
+}
