bca564e2843d29161dcedb514caf97275bfd56e9
aktunc-STMU <aktunc@gmail.com>
2015-01-31 21:40:31 -0600

    A few important suggestions: Look at the comments in the program

--- StockTransaction/src/assignment1.java
+++ StockTransaction/src/assignment1.java
@@ -1,9 +1,9 @@
 
-public class assignment1 {
+public class assignment1 { //A class name should start with a capital letter: Assignment1
 	public static void main(String[] args) {
 		
 		int radius = 5;
-		double area = Math.PI*r*r;
+		double area = Math.PI*r*r; //r should be replaced with radius.
 		int diameter = 2 * radius;
 		
 		System.out.println("The circle's radius is 5, the area is  " +area);
