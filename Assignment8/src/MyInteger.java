58d43335b1af84b6a841005a67a58c2427e643ca
iis1994 <isandoval2@mail.stmarytx.edu>
2015-04-02 22:00:01 -0500

    Assignment 9 (The Modified MyInteger file)

--- Assignment8/src/MyInteger.java
+++ Assignment8/src/MyInteger.java
@@ -1,28 +1,90 @@
 public class MyInteger {
-		int userInteger;
-		int number;
+		int u;
+		static int number;
 		static boolean isEven;
 		static boolean isOdd;
 		static boolean isPrime;
-
-	public boolean getIsEven(int userInteger){
+		int value;
 		
-		if(userInteger%2==0){
+		public MyInteger() {
+			this.value=value;
+		}
+		public int getValue(){
+			return value;
+		}
+		
+		public boolean isEven(){
+			if (value%2==0){
+				return true;
+			}
+			return false;
+		}
+		public boolean isOdd(){
+			if(value%2!=0){
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean isPrime(){
+			for(number=2; number<value; number++){
+				if(value%number==0)
+				return false;
+			}
+			return true;
+		}
+	public static boolean isEven(int u){
+		
+		if(u%2==0){
 			return true;
 		}
 		return false;
 	}
-	public boolean getIsOdd(int userInteger){
-		if(userInteger%2!=0){
+	public static boolean isOdd(int u){
+		if(u%2!=0){
 		return true;
 		}	
 		return false;
 		
 	}
-	public boolean getIsPrime(int userInteger){
-		for(number=2; number<userInteger; number++){
-			if (userInteger%number==0)
+	public static boolean isPrime(int u){
+		for(number=2; number<u; number++){
+			if (u%number==0)
 		return false;}
 		return true;
+	}
+	public boolean isEven(MyInteger u){
+		
+		if(u.getValue()%2==0){
+			return true;
+		}
+		return false;
+	}
+	public boolean isOdd(MyInteger u){
+		if(u.getValue()%2!=0){
+		return true;
+		}	
+		return false;
+		
+	}
+	public boolean isPrime(MyInteger u){
+		for(number=2; number<u.getValue(); number++){
+			if (u.getValue()%number==0)
+		return false;}
+		return true;
+		}
+	public boolean equals(int u){
+		if(value==u){
+			return true;}
+			return false;
+		}
+	public boolean equals(MyInteger u){
+		if(value==u.getValue()){
+			return true;
+		}
+		return false;
+	}
+	public static int parseInt(String u){
+		return Integer.parseInt(u);
 	}
 }
--- Assignment8/src/TestMyInteger.java
+++ Assignment8/src/TestMyInteger.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 public class TestMyInteger {
 	public static void main(String[] args) {
 
-	
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter a positive integer");
-		int userInteger = input.nextInt();
-		
 		MyInteger mi = new MyInteger();
 		
-		System.out.println("isEven: "+mi.getIsEven(userInteger));
-		System.out.println("isOdd: "+mi.getIsOdd(userInteger));
-		System.out.println("isPrime: "+mi.getIsPrime(userInteger));
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int u = input.nextInt();
+		
+		System.out.println("isEven: "+mi.isEven(u));
+		System.out.println("isOdd: "+mi.isOdd(u));
+		System.out.println("isPrime: "+mi.isPrime(u));
+		
+		MyInteger m2 = new MyInteger();
+		System.out.println("Enter another positive integer");
+		int value = input.nextInt();
+		System.out.println("\nisEven: "+MyInteger.isEven(value));
+		System.out.println("isOdd: "+MyInteger.isOdd(value));
+		System.out.println("isPrime: "+MyInteger.isPrime(value));
+	
+		System.out.println("\nEquals? "+m2.equals(mi));
+		System.out.println(MyInteger.parseInt(u));
 	}
 
 }
